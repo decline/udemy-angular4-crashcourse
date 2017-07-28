bf042bc2a9f74890d0afdf45f28fd888374efe92
Dominik Huppmann <dominik.huppmann@textbroker.eu>
2017-07-28 17:30:55 +0200

    add bootstrap / add event handling and (two-way) data-binding

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -9,11 +9,34 @@ import { CourseService } from '../course.service';
 export class CourseComponent implements OnInit {
     courses: string[];
     imageUrl = 'http://lorempixel.com/400/200';
+    isActive = true;
+    emailAddress = 'me@example.com';
 
     constructor(courseService: CourseService) {
         this.courses = courseService.getCourses();
     }
 
     ngOnInit(): void {
+    }
+
+    onSave($event: Event) {
+        $event.stopPropagation();
+        console.log('Button was clicked!', $event);
+    }
+
+    onDivClicked() {
+        console.log('Div was clicked!');
+    }
+
+    onEnterPressed($event) {
+        console.log($event.target.value);
+    }
+
+    onEmailSubmitted(email: String) {
+        console.log(`Email: ${email}`);
+    }
+
+    onEmailTyped() {
+        console.log(this.emailAddress);
     }
 }
