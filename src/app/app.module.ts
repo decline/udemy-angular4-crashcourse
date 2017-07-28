97bc55c0607fa97f706f44337ba21c7b9a344829
dominik.huppmann <dominik.huppmann@gmail.com>
2017-07-28 18:57:48 +0200

    implement "favorite" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { CourseComponent } from './course/course.component';
 import { CourseService } from './course.service';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         CourseComponent,
-        SummaryPipe
+        SummaryPipe,
+        FavoriteComponent
     ],
     imports: [
         BrowserModule,
