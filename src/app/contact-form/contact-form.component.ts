c989a632fb0c7ceca8d7ef66eab057a8a3fa71b8
dominik.huppmann <dominik.huppmann@gmail.com>
2017-07-29 16:04:19 +0200

    implement bootstrap template form

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+    selector: 'app-contact-form',
+    templateUrl: './contact-form.component.html',
+    styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+
+    contactMethods =  [
+        { id: 1, name: 'Email' },
+        { id: 2, name: 'Phone' }
+    ];
+
+    log(x) {
+        console.log(x);
+    }
+
+    submit(form: NgForm) {
+        console.log(form.value);
+    }
+}
